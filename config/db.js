d1d7a0dbeefd43f057c7a41113e6ca426503f3fd
mathertahir <102432999+mathertahir@users.noreply.github.com>
2024-08-08 15:28:52 +0500

    COnnection Changes Revert

--- config/db.js
+++ config/db.js
@@ -1,33 +1,12 @@
-// const mongoose = require("mongoose");
-
-// const dbConnect = async () => {
-//   try {
-//     const con = await mongoose.connect(process.env.MONGO_ENV);
-
-//     console.log("connected db");
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// module.exports = dbConnect;
-
 const mongoose = require("mongoose");
 
 const dbConnect = async () => {
   try {
-    const con = await mongoose.connect(process.env.MONGO_ENV, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000, // Adjusts how long to wait for a connection to be established
-      socketTimeoutMS: 45000, // Adjusts how long Mongoose should wait before timing out
-      autoReconnect: true, // Reconnect on connection loss
-    });
+    const con = await mongoose.connect(process.env.MONGO_ENV);
 
-    console.log(`Connected to MongoDB: ${con.connection.host}`);
+    console.log("connected db");
   } catch (error) {
-    console.error(`Error connecting to MongoDB: ${error.message}`);
-    process.exit(1); // Optional: Exit the process if connection fails
+    console.log(error);
   }
 };
 
